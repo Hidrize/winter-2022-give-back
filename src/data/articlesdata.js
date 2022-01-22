c7ddc8b3efa2234c4cdf7b7901d9327adc1f0135
Michael Ellis <computerscimike@gmail>
2022-01-21 19:38:03 -0500

    starting on final project

---
+++ src/data/articlesdata.js
@@ -0,0 +1,21 @@
+function Article(){
+
+    return(
+        <div>
+        <div>
+            <h3>Bedbound Patients</h3>
+            <img src ='https://cdn.flintrehab.com/uploads/2021/08/doctor-helping-bedridden-stroke-patient.jpg' alt='img' />
+        </div>
+        <div> 
+            <h3>Treating Patients</h3>
+            <img src ='https://dam.northwell.edu/m/78b87fcc218f1673/Drupal-NEWS_Can-doctors-refuse-to-treat-a-patient.jpg' alt='img' />
+        </div>
+        <div>
+            <h3>Wheel Chair Bound Patients</h3>
+            <img src ='https://media.gettyimages.com/photos/mature-doctor-explains-treatment-plan-to-wheelchair-bound-patient-picture-id1182706350?s=612x612' alt='img' />
+
+        </div>
+        </div>
+    );
+}
+   export default Article;
