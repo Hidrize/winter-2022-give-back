ce91c60b73c17edfe54e21bbedd34d48485be837
Hidrize <Olanilekan@gmail.com>
2022-01-25 11:22:09 -0500

    document format

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import About from './components/About-us';
 import Testimonial from './components/Testimonial';
 import Donate from './components/Donate';
 import Home from './components/Home';
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
 
- 
-    return (
-      <BrowserRouter>
-        <Nav />
-          <Routes >
-            <Route path="/Components/Home" exact element={<Home/>}/>
-            <Route path="/Components/About-us"   exact element={<About/>}/>
-            <Route path="/Components/Testimonial" exact element={<Testimonial/>}/>
-    `       <Route path="/Components/Donate"     exact element={<Donate/>}/>
-          </Routes>
-      </BrowserRouter>
-    
-    )
-   }
-   
+
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes >
+        <Route path="/Components/Home" exact element={<Home />} />
+        <Route path="/Components/About-us" exact element={<About />} />
+        <Route path="/Components/Testimonial" exact element={<Testimonial />} />
+        <Route path="/Components/Donate" exact element={<Donate />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
 
 export default App;
