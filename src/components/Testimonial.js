234aa28427b4a7cd53ae0aa157cb10a79952c6f6
Tony C <tonyc@Tonys-MacBook-Pro.local>
2022-01-26 15:28:42 -0500

    Finished Testimonial layout

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,18 +1,37 @@
-import testimonialData from "../Data/testimonialdata";
+import { Card, CardMedia, Typography, CardContent,Grid} from '@mui/material';
+import React from 'react';
+import testimonialData from '../Data/testimonialdata';
+const TestimonialCard = ({title, picture, body}) => {
+  return(
+          <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                  component="img"
+                  height="140"
+                  image={picture}
+                  alt={title}
+                  />
+                  <CardContent>
+                          <Typography gutterBottom variant='h5' component="div">
+                                  {title}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                                  {body}
+                          </Typography>
+                  </CardContent>
 
-function Testimonial() {
-        return (
-                <div>
-                        {testimonialData.map(testimonialbox)}
-
-                </div>
+          </Card>
+  );
+};
+const Testimonial=()=> {
+        return(
+                <Grid container spacing={4}>
+                        {testimonialData.map((data)=>(
+                              <Grid item xs={3}>
+                                        <TestimonialCard picture= {data.picture} title={data.title} body={data.body}/>
+                                </Grid>
+                                )
+                        )}
+                </Grid>
         )
 }
 export default Testimonial;
-
-function testimonialbox(testimonialdata){
-
-        return (
-                <h1>testimonialdata.title</h1>
-        )
-}
